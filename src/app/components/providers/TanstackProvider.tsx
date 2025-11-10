539a3486912a1331ec90c356f060ff8571795c48
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-11-11 00:19:38 +0530

    feat: added tanstack provider

---
+++ src/app/components/providers/TanstackProvider.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React  from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function makeQueryClient(){
+    return new QueryClient({
+        defaultOptions:{
+            queries:{
+                staleTime: 60*1000
+            }
+        }
+    })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+function getQueryClient(){
+    if(typeof window === "undefined"){
+
+        return makeQueryClient();
+    }else{
+        if(!browserQueryClient){
+            browserQueryClient = makeQueryClient();
+        }
+        return browserQueryClient;
+    }
+}
+
+export default function TanstackProvider({ children}:{children: React.ReactNode}){
+    const queryClient = getQueryClient();
+    return <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        {children}
+        </QueryClientProvider>
+}

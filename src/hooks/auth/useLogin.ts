b69e01c1c4e9cf766b4a8fd6ddd6bb48e28a7c72
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-11-12 00:20:08 +0530

    feat: added useLogin hook

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,2 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
-type LoginRequest = 
+import { login } from "@/api/auth";
+import type { components } from "@/types/api";
+
+type LoginRequest = components["schemas"]["LoginRequest"];
+type LoginResponse = components["schemas"]["LoginResponse"];
+
+export function useLogin(){
+    return useMutation<LoginResponse,Error,LoginRequest>({
+        mutationFn:(data)=>login(data),
+    })
+}
